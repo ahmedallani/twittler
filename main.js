e0006ad88a6706d57b3c5c33b3e2f30dcf399bf9
allania7med11 <allania7med11@gmail.com>
2021-03-14 14:49:00 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+function refresh(arr) {
+  var $tweets = $("#tweets");
+  $tweets.html("");
+  if(arr===undefined){
+    arr = streams.home
+  }
+  
+  var index = arr.length - 1;
+  while (index >= 0) {
+    var tweet = arr[index];
+    var $tweet = $("<div></div>");
+    var html="<div class='user'>"+tweet.user+"</div>"
+    html+="<div class='message'>"+tweet.message+"</div>"
+    html += "<div class='time'>" + moment(tweet.created_at).fromNow() + "</div>";
+    $tweet.html(html);
+    $tweet.appendTo($tweets);
+    index -= 1;
+  }
+  $(".user").unbind().click(function () {
+    var user = this.innerHTML
+    var arr = filter(streams.home,function(tweet){
+      return tweet.user === user
+    })
+    refresh(arr)
+  });
+}
+$(document).ready(function () {
+    refresh()
+});
